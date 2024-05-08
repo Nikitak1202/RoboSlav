3c8d45a084b352cb4623dc9885c31da20f58688d
Nikitak1202 <Nikitak1202@yandex.ru>
2024-05-08 17:08:24 +0300

    IMU read

--- IMU/get_IMU_data.py
+++ IMU/get_IMU_data.py
@@ -1,8 +1,9 @@
 from madgwickahrs import MadgwickAHRS
 from pytroykaimu import TroykaIMU
- 
-imu = TroykaIMU()
 
+
+"""
+imu = TroykaIMU()
 # Чтобы пример работал корректно, нужно предварительно получить 
 # калибровочную матрицу для вашего модуля
 # Как это сделать описано в разделе "Калибровка магнитометра"
@@ -12,17 +13,21 @@ calibration_matrix = [[0.983175, 0.022738, -0.018581],
 
 # raw measurements only
 bias = [962.391696, -162.681348, 11832.188828]
+"""
 
-imu.magnetometer.calibrate_matrix(calibration_matrix, bias)
 
-filter = MadgwickAHRS(beta=1, sampleperiod=1/256)
+def readIMU(imu, calibration_matrix, bias):
+    imu.magnetometer.calibrate_matrix(calibration_matrix, bias)
 
-while True:
+    filter = MadgwickAHRS(beta=1, sampleperiod=1/256)
+
     filter.update(imu.gyroscope.read_radians_per_second_xyz(),
-                  imu.accelerometer.read_gxyz(),
-                  imu.magnetometer.read_calibrate_gauss_xyz())
+                    imu.accelerometer.read_gxyz(),
+                    imu.magnetometer.read_calibrate_gauss_xyz())
     data = filter.quaternion.to_angle_axis()
 
     dataencode = str(data).encode('utf-8')
     if dataencode:
         print(data)
+    
+    return
